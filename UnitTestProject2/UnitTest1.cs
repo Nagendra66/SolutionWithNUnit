9668862ff7d845bf45aa1be83aac4e3e5a7309fc
Nagendra <dnagendra@mirabeltechnologies.com>
2018-03-26 14:53:24 +0530

    mar26_1

---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit;
+using NUnit.Framework;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int actual, expected = 30;
+            BAL.BalClass obj = new BAL.BalClass();
+            actual = obj.AddTwoNumbers(12, 18);
+            NUnit.Framework.Assert.AreEqual(expected, actual);
+        }
+    }
+}
